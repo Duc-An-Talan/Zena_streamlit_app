f1be6c334544a001f277b4b1983d9e46463d9cb0
Duc-An-Talan <131973274+Duc-An-Talan@users.noreply.github.com>
2023-05-05 15:51:30 +0200

    Update zena_streamlit_app.py

--- zena_streamlit_app.py
+++ zena_streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
+"""import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError"""
 
+streamlit.title("Zena Shop")
 
-#streamlit.title("Zena Shop")
-streamlit.title("My Mom' New Healthy Diner ")
-streamlit.header('Breakfast Menu')
